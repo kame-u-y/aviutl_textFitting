a19652915a60c611adae1a8b528b06f645ee3948
kame-v-d <ffmstortoise62002@gmail.com>
2020-08-11 15:49:09 +0900

    個別オブジェクトはなかったことに

--- src/テキストサイズ自動調節.lua
+++ src/テキストサイズ自動調節.lua
@@ -8,35 +8,12 @@ local zScale = (camZ - obj.z) / camZ
 local margin = 100
 local screenW = obj.screen_w - margin
 local zoomRate = obj.getvalue("zoom")/100
---local thresRate = obj.track0/100
+--local thresRate = 100/obj.track0
 local thresRate = 1.0
-local thresholdW = screenW * zoomRate / thresRate
+local thresholdW = screenW * zoomRate * thresRate
 
-local isLast = false
-local wSum = 0
-
-
---サイズ取得
-if (obj.getoption("multi_object")==true) then
-    hoge = "piyo"
-    wSum = wSum + obj.w
+local objectW = obj.w
+if (objectW < thresholdW) then
+    objectW = thresholdW
 end
-
---ラスト判定とサイズ調節
-if (obj.index == obj.num-1) then
-    isLast = true
-end
-
-if (isLast) then
-    fuka = wSum
-    local objectW = 0
-    if (obj.getoption("multi_object")) then
-        objectW = wSum
-    else
-        objectW = obj.w
-    end
-    if (objectW < thresholdW) then
-        objectW = thresholdW
-    end
-    obj.zoom = screenW * zScale / objectW
-end
+obj.zoom = screenW * zScale / objectW
