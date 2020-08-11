01ce2ad7b40f1013cb417804e6f0e3df6e30fccc
kame-v-d <ffmstortoise62002@gmail.com>
2020-08-11 15:33:08 +0900

    失敗 個別オブジェクト試行錯誤

--- src/テキストサイズ自動調節.lua
+++ src/テキストサイズ自動調節.lua
@@ -1,17 +1,42 @@
 --track0:拡大率,1,1000,100,1
 --track1:余白,0,9999,0,1
 
-camZ = -1024
-zScale = (camZ - obj.z) / camZ
+local camZ = -1024
+local zScale = (camZ - obj.z) / camZ
 
-screenW = obj.screen_w - obj.track1
-zoomRate = obj.getvalue("zoom")/100
-thresRate = obj.track0/100
-thresholdW = screenW * zoomRate / thresRate
+--local margin = obj.track1
+local margin = 100
+local screenW = obj.screen_w - margin
+local zoomRate = obj.getvalue("zoom")/100
+--local thresRate = obj.track0/100
+local thresRate = 1.0
+local thresholdW = screenW * zoomRate / thresRate
 
-objectW = obj.w
-if (obj.w < thresholdW) then
-    objectW = thresholdW
+local isLast = false
+local wSum = 0
+
+
+--サイズ取得
+if (obj.getoption("multi_object")==true) then
+    hoge = "piyo"
+    wSum = wSum + obj.w
 end
 
-obj.zoom = screenW * zScale / objectW
+--ラスト判定とサイズ調節
+if (obj.index == obj.num-1) then
+    isLast = true
+end
+
+if (isLast) then
+    fuka = wSum
+    local objectW = 0
+    if (obj.getoption("multi_object")) then
+        objectW = wSum
+    else
+        objectW = obj.w
+    end
+    if (objectW < thresholdW) then
+        objectW = thresholdW
+    end
+    obj.zoom = screenW * zScale / objectW
+end
